36f33e40516dd87716c88912548931cf82ecaa68
Ona <onushkis@gmail.com>
2022-01-28 01:30:52 +0100

    added Tailwind.css

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,32 @@
+/** @jsxImportSource @emotion/react */
+
+import { jsx, css, useTheme } from "@emotion/react";
+
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
-const Home = () => {
+const Home = (props) => {
+  const theme = useTheme();
+  const styles = css`
+  .heading {
+    font-size: 20px;
+    color: ${theme.colors.primary};
+    margin-bottom: 10px;
+    cursor: pointer;
+    &:hover {
+      color: blue;
+    }
+  }
+  `;
   const { error, isPending, data: blogs } = useFetch('http://localhost:3000/blogs')
 
   return (
+    <div css={styles}>
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} /> }
+    </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'regal-red': '#7f1d1d',
+      },
+      
+    },
+  },
+  plugins: [],
+}
